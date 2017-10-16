212dcfd0b615dc187a8f0970308cacfa27be533b
Josh Tucholski <josh@techelevator.com>
2017-10-16 13:39:45 -0400

    Added colorized result exercise.

---
+++ m3-w8d1-controllers-part1-exercises/Exercises.Web/Controllers/ColorizerController.cs
@@ -0,0 +1,25 @@
+﻿using Exercises.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercises.Web.Controllers
+{
+    public class ColorizerController : Controller
+    {
+        // GET: Colorizer
+        public ActionResult ColorizedForm()
+        {
+            ColorizerModel model = new ColorizerModel();
+            return View("ColorizedForm", model); //pass the model into the view
+        }
+
+        public ActionResult ColorizedFormResult(ColorizerModel input)
+        {
+            ColorizerModel model = input;
+            return View("ColorizedFormResult", model);
+        }
+    }
+}
---
+++ m3-w8d1-controllers-part1-exercises/Exercises.Web/Models/ColorizerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exercises.Web.Models
+{
+    public class ColorizerModel
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string MiscellaneousColor { get; set; }
+
+        [Display(Name = "Red")]
+        public bool IsRed { get; set; }
+        public bool IsBlue { get; set; }
+        public bool IsGreen { get; set; }
+    }
+}
